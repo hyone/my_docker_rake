4a1d30f09b8d7ffcd82d5a0d262e0b6515f35e30
hyone <hyone.development@gmail.com>
2014-03-10 18:27:42 +0900

    default hostname is now the container name that is replaced '.' with '_'

--- lib/my_docker_rake/tasks.rb
+++ lib/my_docker_rake/tasks.rb
@@ -73,7 +73,7 @@ module MyDockerRake
               sh <<-EOC.gsub(/\s+/, ' ')
                 docker run -d \
                   --name #{container[:name]} \
-                  --hostname #{container[:hostname] || container[:name]} \
+                  --hostname #{container[:hostname] || container[:name].gsub(/\./, '_')} \
                   #{ links.map { |l| "--link #{l}" }.join(' ') } \
                   #{ ports.map { |p| "-p #{p}" }.join(' ') } \
                   #{ volumes_from.map { |v| "--volumes-from #{v}" }.join(' ') } \
